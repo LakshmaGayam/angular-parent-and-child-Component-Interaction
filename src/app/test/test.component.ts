84db4e0388ac85078c27676bed5b825124b2f84b
lakshmareddy <51111017+LakshmaGayam@users.noreply.github.com>
2019-08-02 11:40:34 +0530

    Update test.component.ts

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit,Input,Output ,EventEmitter} from '@angular/core';
 export class TestComponent implements OnInit {
 //this line is to get data from parent component (we have to use decorator called @input() to the reference variable)
   @Input()public ParentData;
+  // at line no 12 if we want send data from child to parent we have to create an object for EventEmitter() through that object refernce 
+  //we can call the data to the parent.In parent component we have to bind the property
+
 @Output()  public Childevent=new EventEmitter()
 
   constructor() { }
